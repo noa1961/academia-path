f8ee0b0cf4ade088c1f19712b9cd4e158dd5b222
Tiago Calado <1tiagocalado8@gmail.com>
2023-01-19 15:42:53 +0000

    [Views] Make UI changes

--- views/colleges.create.view.php
+++ views/colleges.create.view.php
@@ -7,11 +7,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-  <div class="container-fluid">
-    <h1>New College</h1>
+  <div class="container">
+    <h1 class="mt-5">New College</h1>
     <form method="POST" action="<?php echo route('colleges'); ?>">
       <div class="form-group">
-        <input type="text" class="form-control" name="nome" placeholder="Input the college name" required>
+        <input type="text" class="form-control" name="name" placeholder="Input the college name" required>
       </div>
       <div class="form-group">
         <select class="custom-select" name="type_id">
@@ -27,7 +27,19 @@
       <div class="form-group">
         <input type="text" class="form-control" name="ranking" placeholder="Insert the college ranking" required>
       </div>
-        <button type="submit" class="btn btn-primary">Add</button>
+      <div class="from-group">
+        <input type="text" class="form-control" name="description" placeholder="Insert the college description">
+      </div>
+        <div class="from-group">
+            <input type="number" class="form-control mt-3" name="acceptanceRate" placeholder="Insert the college Acceptance Rate">
+        </div>
+        <div class="from-group">
+            <input type="number" class="form-control mt-3" name="graduationRate" placeholder="Insert the college Graduation  Rate">
+        </div>
+        <div class="from-group">
+            <input type="number" class="form-control mt-3" name="tuitionCost" placeholder="Insert the college Tuition Cost">
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Add</button>
     </form>
     </div>
 </body>
