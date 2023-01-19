ebc664660fa1eb3f0d359dac74cc9a88eda8d186
Tiago Calado <1tiagocalado8@gmail.com>
2023-01-19 15:37:44 +0000

    [Routes] Fix a route typo

--- routes.php
+++ routes.php
@@ -29,7 +29,7 @@ $router->post('colleges', function() {
 });
 
 // edit colleges
-$router->get('collcollegesege/(\d+)/edit', function($id) {
+$router->get('colleges/(\d+)/edit', function($id) {
     require 'controllers/colleges.edit.php';
 });
 $router->patch('colleges/(\d+)', function($id) {
