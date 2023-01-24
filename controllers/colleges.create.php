ee1466dfb058827b37a0d32d487cb6323ff23402
Tiago Calado <1tiagocalado8@gmail.com>
2023-01-24 15:37:25 +0000

    Implement insert college visuals

--- controllers/colleges.create.php
+++ controllers/colleges.create.php
@@ -7,5 +7,6 @@ $connection = Connection::make();
 $queryBuilder = new QueryBuilder($connection);
 
 $types = $queryBuilder->getAll('type','App\Model\Type');
+$locations = $queryBuilder->getAll('location','App\Model\Location');
 
 require 'views/colleges.create.view.php';
--- views/colleges.create.view.php
+++ views/colleges.create.view.php
@@ -10,9 +10,50 @@
   <div class="container">
     <h1 class="mt-5 mb-5">New College</h1>
     <form method="POST" action="<?php echo route('colleges'); ?>">
+
+      <!-- College Name -->
       <div class="form-group">
         <input type="text" class="form-control" name="name" placeholder="Input the college name" required>
       </div>
+
+      <!-- College Description -->
+      <div class="from-group mb-3">
+        <input type="text" class="form-control" name="description" placeholder="Insert the college description">
+      </div>
+
+      <!-- College Acceptance Rate -->
+      <div class="from-group">
+          <input type="number" class="form-control mt-3" name="acceptanceRate" placeholder="Insert the college Acceptance Rate">
+      </div>
+
+      <!-- College Graduation Rate -->
+      <div class="from-group">
+          <input type="number" class="form-control mt-3 mb-3" name="graduationRate" placeholder="Insert the college Graduation  Rate">
+      </div>
+
+      <!-- College Tuition Cost -->
+      <div class="from-group">
+          <input type="number" class="form-control mt-3 mb-3" name="tuitionCost" placeholder="Insert the college Tuition Cost">
+      </div>
+
+      <!-- College Ranking -->
+      <div class="form-group">
+        <input type="number" class="form-control" name="ranking" placeholder="Insert the college ranking" required>
+      </div>
+
+      <!-- College Logo -->
+      <div class="custom-file mb-3">
+        <input type="file" class="custom-file-input" name="collegeLogo" required>
+        <label class="custom-file-label" style="color: gray;" for="validatedCustomFile">Upload a College Logo...</label>
+      </div>
+
+      <!-- College Image -->
+      <div class="custom-file mb-3">
+        <input type="file" class="custom-file-input" name="collegeImage" required>
+        <label class="custom-file-label" style="color: gray;" for="validatedCustomFile">Upload a College Image...</label>
+      </div>
+
+      <!-- College Type -->
       <div class="form-group">
         <select class="custom-select" name="type_id">
           <?php
@@ -24,21 +65,20 @@
           ?>
         </select>
       </div>
+
+      <!-- College Location -->
       <div class="form-group">
-        <input type="text" class="form-control" name="ranking" placeholder="Insert the college ranking" required>
+        <select class="custom-select" name="location_id">
+          <?php
+            foreach($locations as $location) {
+              ?>
+              <option value="<?php echo $location->id; ?>"><?php echo $location->state; ?></option>
+              <?php
+            }
+          ?>
+        </select>
       </div>
-      <div class="from-group">
-        <input type="text" class="form-control" name="description" placeholder="Insert the college description">
-      </div>
-        <div class="from-group">
-            <input type="number" class="form-control mt-3" name="acceptanceRate" placeholder="Insert the college Acceptance Rate">
-        </div>
-        <div class="from-group">
-            <input type="number" class="form-control mt-3" name="graduationRate" placeholder="Insert the college Graduation  Rate">
-        </div>
-        <div class="from-group">
-            <input type="number" class="form-control mt-3" name="tuitionCost" placeholder="Insert the college Tuition Cost">
-        </div>
+
         <button type="submit" class="btn btn-dark mt-3">Add</button>
     </form>
     </div>
