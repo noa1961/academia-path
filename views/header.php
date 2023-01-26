6d6eb57c276ac064f217ca444125e7d6dd43d72b
Tiago Calado <1tiagocalado8@gmail.com>
2023-01-26 14:44:33 +0000

    [Homepage] Build header generalization

---
+++ views/header.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a href="<?php echo route('home'); ?>" class="navbar-brand mb-0 h1"><?php echo $appName; ?></a>
+        </div>
+    </nav>
+    <a class="btn btn-lg btn-outline-dark" href="<?php echo route('colleges'); ?>">Admin View</a>
+</header>
