3cc6435b5b137d99ec2f6311fda44dd696bf1f44
Tiago Calado <1tiagocalado8@gmail.com>
2023-01-26 14:46:22 +0000

    [Homepage] Build header generalization

--- views/home.view.php
+++ views/home.view.php
@@ -6,14 +6,7 @@
   <title>Academia Path</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
+    <?php include 'header.php'; ?>
     <body>
-        <div>
-            <div class="navbar navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1"><?php echo $appName; ?></span>
-                </div>
-            </div>
-            <a class="btn btn-lg btn-outline-dark" href="<?php echo route('colleges'); ?>">Admin View</a>
-        </div>
     </body>
 </html>
