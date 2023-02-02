f530c188b5dba1d02d731c49076336a61cdbcc0d
Tiago Calado <1tiagocalado8@gmail.com>
2023-02-02 17:12:01 +0000

    [Files] Delete images files after deleting college

--- controllers/colleges.delete.php
+++ controllers/colleges.delete.php
@@ -8,4 +8,7 @@ $queryBuilder = new QueryBuilder($connection);
 
 $college = $queryBuilder->deleteById('college',$id);
 
+unlink("img/upload/campus/$id.png");
+unlink("img/upload/logos/$id.png");
+
 redirect('colleges');
