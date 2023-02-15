ec22817cec0eb40d9a9f1a9260016309bdaf26c3
Tiago Calado <1tiagocalado8@gmail.com>
2023-02-15 22:56:30 +0000

    [Routes] Make login and register get and post routes

--- routes.php
+++ routes.php
@@ -1,26 +1,36 @@
 <?php
 
-// Home page
+// HomePage
 $router->get('', function() {
     require 'controllers/home.php';
 });
 
-// List colleges
+// List Colleges *USER*
+$router->get('colleges', function() {
+    require 'controllers/collegesUser.index.php';
+});
+
+// List Colleges *ADMIN*
 $router->get('admin/colleges', function() {
     require 'controllers/colleges.index.php';
 });
 
-// Show colleges with id
+// Show Colleges with id
 $router->get('colleges/(\d+)', function($id) {
     require 'controllers/colleges.show.php';
 });
 
-// Delete colleges
+// List Colleges by Location id
+$router->get('locations/(\d+)', function($id) {
+    require 'controllers/locations.show.php';
+});
+
+// Delete Colleges
 $router->delete('admin/colleges/(\d+)', function($id) {
     require 'controllers/colleges.delete.php';
 });
 
-// Add colleges
+// Add Colleges
 $router->get('admin/colleges/create', function() {
     require 'controllers/colleges.create.php';
 });
@@ -28,7 +38,7 @@ $router->post('admin/colleges', function() {
     require 'controllers/colleges.store.php';
 });
 
-// Edit colleges
+// Edit Colleges
 $router->get('admin/colleges/edit/(\d+)', function($id) {
     require 'controllers/colleges.edit.php';
 });
@@ -36,4 +46,25 @@ $router->patch('admin/colleges/(\d+)', function($id) {
     require 'controllers/colleges.update.php';
 });
 
-// Login user
+// Login User
+$router->get('login', function() {
+    require 'controllers/login/login.php';
+});
+
+$router->post('login', function() {
+    require 'controllers/login/process.php';
+});
+
+$router->get('logout', function() {
+    unset($_SESSION['user']);
+    unset($_SESSION['user_id']);
+    redirect('');
+});
+
+// Register User
+$router->get('register', function() {
+    require 'controllers/register/create.php';
+});
+$router->post('register', function() {
+    require 'controllers/register/store.php';
+});
